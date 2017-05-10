a6cb76506494c1661a370e519b6216b37ca43a09
Charles Burgess <consulting@cvburgess.com>
2017-05-09 22:16:54 -0400

    add route for create user

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const validator = require('validator');
 
 const port = process.env.PORT;
+const secretKey = process.env.SECRET_KEY;
+const databaseUrl = process.env.DATABASE_URL;
 
 const app = express();
 const db = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL
+  connection: databaseUrl
 });
 
 app.disable('x-powered-by');
@@ -15,6 +18,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Unscripted Cagematch API is live'));
+
+// Create new user
+app.post('/users', bodyParser.json(), (req, res) => {
+  const { email, hasOptedIn } = req.body;
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: `${email} is not a valid email address` });
+  }
+
+  return knex.insert({ email, hasOptedIn }).into('users').returning('*')
+  .then(userData => res.json( user ))
+  .catch(error => res.status(400).json({ error }));
+});
+
+// MATCHES - SK required
+// TEAMS - SK required
+// VOTES - User required
 
 app.listen(port);
 console.log(`Worker listening on :${port}`);
